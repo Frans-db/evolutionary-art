65f86d8db8455a8eada1c9e5fcffc5c40254dfe2
Frans de Boer <frans-de-boer@hotmail.com>
2023-09-21 09:35:11 +0200

    2 basic taglists

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--taglist', type=str)
+
+    return parser.parse_args()
+
+def load_taglist(path: str):
+    with open(path) as f:
+        data = f.read()
+
 def main():
-    print('main')
+    args = parse_args()
+    with open(args.taglist) as f:
+        data = f.read()
+    print(data)
+    
 
 if __name__ == '__main__':
     main()

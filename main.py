2addef2945bc5ac8b8f7dded18b7cc5999e111b9
Frans de Boer <frans-de-boer@hotmail.com>
2023-09-21 10:09:44 +0200

    classlist loading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--classlist', type=str)
@@ -9,15 +10,29 @@ def parse_args():
 def load_classlist(path: str):
     with open(path) as f:
         data = f.read()
+
+    classes = []
     for line in data.split('\n'):
-        split_point = line.find('-')
+        split_point = line.rfind('-')
         class_name = line[:split_point]
         values = line[split_point+1:]
-        print(class_name, values)
+        
+        # load a sequence of classess
+        if values.startswith('['):
+            values = values[1:-1]
+            for value in values.split(','):
+                classes.append(f'{class_name}-{value}')
+            continue
+        
+        # load a single class
+        classes.append(f'{class_name}-{values}')
+    
+    return classes
 
 def main():
     args = parse_args()
-    tags = load_classlist(args.classlist)
+    classes = load_classlist(args.classlist)
+    print(classes)
     
 
 if __name__ == '__main__':

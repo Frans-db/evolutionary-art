623371dd8dbf3ef6702b70fa821d35e4b86373a3
Frans de Boer <frans-de-boer@hotmail.com>
2023-09-27 16:30:28 +0200

    element rendering, started experiment

--- evweb/elements/elements.py
+++ evweb/elements/elements.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass, field
+from html2image import Html2Image
+import os
 
 from .properties import Property, RealProperty, DiscreteProperty, RGBProperty
 
@@ -8,7 +10,12 @@ class Element:
     tag: str
     children: list["Element"] = field(default_factory=list)
     properties: list[Property] = field(default_factory=list)
-    contents: str = ''
+    contents: str = ""
+    # Fitness is only used on the root element.
+    # Another way to do this would be to create an Individual object
+    # which stores an element and its fitness. This would be the only
+    # purpose for the individual though, so I prefer this.
+    fitness: int = -1
 
     def __post_init__(self) -> None:
         displays = ["block", "inline", "flex"]
@@ -30,12 +37,19 @@ class Element:
         inner = "\n".join([self.contents] + children_html)
         return f'<{self.tag} style="{style}">{inner}</{self.tag}>'
 
+    def render(self, directory: str, filename: str) -> None:
+        html = self.to_html()
+        renderer = Html2Image(output_path=directory)
+        renderer.screenshot(html=html, save_as=f"{filename}.png")
+        with open(os.path.join(directory, f"{filename}.html"), "w+") as f:
+            f.write(html)
+
 
 def get_default_element() -> Element:
     root = Element("div")
     for _ in range(5):
-        child = Element('div')
-        child.children.append(Element('div'))
+        child = Element("div")
+        child.children.append(Element("div"))
         root.children.append(child)
 
     return root
--- evweb/evolution/experiment.py
+++ evweb/evolution/experiment.py
@@ -1,6 +1,43 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Callable
+import os
+
+from ..elements import Element, get_default_element
+
 
 @dataclass
 class Experiment:
     root: str
     name: str
+    population_size: int
+    current_generation: int = 0
+    experiment_root: str = None
+    population: list[Element] = None
+
+    evaluate: Callable[[Element], float]
+
+    def __post_init__(self) -> None:
+        self.experiment_root = os.path.join(self.root, self.name)
+        os.makedirs(self.experiment_root, exist_ok=True)
+
+    def run(self) -> None:
+        population = [get_default_element() for _ in range(self.population_size)]
+        for individual_index, individual in enumerate(population):
+            render_dir = os.path.join(self.experiment_root, self.current_generation)
+            individual.render(render_dir, str(individual_index))
+            individual.fitness = self.evaluate(individual)
+
+        # create population
+        # evaluate population
+        # evaluate individual
+        #   render individual
+        #       individual doesnt know where to store result
+        #       experiment does
+        #       add render step?
+        # compare to target
+        # for each generation
+        #   crossover
+        #   mutate
+        #   evaluate
+        #   selection
+        pass
